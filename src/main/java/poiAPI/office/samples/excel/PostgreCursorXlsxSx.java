b7c669f328531319d6fdc006a0b7f84b605a96e0
kharo1963 <kharo1963@gmail.com>
2024-01-07 20:29:51 +0300

    newResultSet = (ResultSet) newInstanceObj

--- src/main/java/poiAPI/office/samples/excel/PostgreCursorXlsxSx.java
+++ src/main/java/poiAPI/office/samples/excel/PostgreCursorXlsxSx.java
@@ -2,7 +2,9 @@ package poiAPI.office.samples.excel;
 
 import jakarta.persistence.EntityManager;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -38,7 +40,7 @@ public class PostgreCursorXlsxSx {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM poi_person");
 
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-
+            System.out.println("============================resultSetValue====================");
             Class resultSetClass = resultSet.getClass();
             Field currentRowField = resultSetClass.getDeclaredField("currentRow");
             currentRowField.setAccessible(true);
@@ -64,6 +66,70 @@ public class PostgreCursorXlsxSx {
                 }
                 System.out.println("bytes " + bytes);
             }
+            System.out.println("============================Constructor Start====================");
+            Constructor<?>[] constructors = resultSetClass.getDeclaredConstructors();
+            System.out.println("constructors " + constructors.length + " " + constructors);
+            for (Constructor<?> constructor : constructors) {
+                System.out.println("constructor " + constructor);
+            }
+//            public ResultSet createResultSet(Query originalQuery, Field[] fields, List<byte[][]> tuples,
+//                    ResultCursor cursor) throws SQLException {
+//                PgResultSet newResult = new PgResultSet(originalQuery, this, fields, tuples, cursor,
+//                        getMaxRows(), getMaxFieldSize(), getResultSetType(), getResultSetConcurrency(),
+//                        getResultSetHoldability());
+//                newResult.setFetchSize(getFetchSize());
+//                newResult.setFetchDirection(getFetchDirection());
+//                return newResult;
+//            }
+            Object[] params = new Object[11];
+            Field originalQueryField = resultSetClass.getDeclaredField("originalQuery");
+            originalQueryField.setAccessible(true);
+            params[0] = originalQueryField.get(resultSet);
+            params[1] = statement;
+            Field fieldsField = resultSetClass.getDeclaredField("fields");
+            fieldsField.setAccessible(true);
+            params[2] = fieldsField.get(resultSet);
+            Field tuplesField = resultSetClass.getDeclaredField("rows");
+            tuplesField.setAccessible(true);
+            params[3] = tuplesField.get(resultSet);
+            Field cursorField = resultSetClass.getDeclaredField("cursor");
+            cursorField.setAccessible(true);
+            params[4] = cursorField.get(resultSet);
+            Field maxRowsField = resultSetClass.getDeclaredField("maxRows");
+            maxRowsField.setAccessible(true);
+            params[5] = maxRowsField.get(resultSet);
+            Field maxFieldSizeField = resultSetClass.getDeclaredField("maxFieldSize");
+            maxFieldSizeField.setAccessible(true);
+            params[6] = maxFieldSizeField.get(resultSet);
+            Field resultsettypeField = resultSetClass.getDeclaredField("resultsettype");
+            resultsettypeField.setAccessible(true);
+            params[7] = resultsettypeField.get(resultSet);
+            Field resultsetconcurrencyField = resultSetClass.getDeclaredField("resultsetconcurrency");
+            resultsetconcurrencyField.setAccessible(true);
+            params[8] = resultsetconcurrencyField.get(resultSet);
+//            Field rsHoldabilityField = resultSetClass.getDeclaredField("rsHoldability");
+//            rsHoldabilityField.setAccessible(true);
+            params[9] = 0;
+            Field adaptiveFetchField = resultSetClass.getDeclaredField("adaptiveFetch");
+            adaptiveFetchField.setAccessible(true);
+            params[10] = adaptiveFetchField.get(resultSet);
+            constructors[0].setAccessible(true);
+            Object newInstanceObj = constructors[0].newInstance(params);
+            ResultSet newResultSet = (ResultSet) newInstanceObj;
+            int k = 0;
+            while (newResultSet.next()) {
+
+                System.out.print(newResultSet.getRow());
+                for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
+                    System.out.print(" " + newResultSet.getObject(i));
+                }
+                System.out.println("");
+                ++k;
+                if (k > 1) {
+                    break;
+                }
+            }
+            System.out.println("============================Constructor End====================");
             for (int i = 1; i <= resultSetMetaData.getColumnCount(); i++) {
                 System.out.println(String.format("-- Column %d --", i));
                 System.out.println(String.format("Column name: %s", resultSetMetaData.getColumnName(i)));
@@ -85,6 +151,10 @@ public class PostgreCursorXlsxSx {
         } catch (NoSuchFieldException e) {
             throw new RuntimeException(e);
         } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
             throw new RuntimeException(e);
         }
     }
